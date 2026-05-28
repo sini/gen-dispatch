eafb09fc9c4762557012666a7ae60bcfe41d89f8
Jason Bowman <jason@json64.dev>
2026-05-28 13:30:27 -0700

    refactor: remove CI input follows, resolved transitively via gen

--- ci/flake.nix
+++ ci/flake.nix
@@ -4,12 +4,6 @@
     gen-algebra.url = "github:sini/gen-algebra";
     gen-select.url = "github:sini/gen-select";
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    flake-parts.follows = "gen/flake-parts";
-    flake-root.follows = "gen/flake-root";
-    nix-unit.follows = "gen/nix-unit";
-    treefmt-nix.follows = "gen/treefmt-nix";
-    devshell.follows = "gen/devshell";
-    import-tree.follows = "gen/import-tree";
   };
 
   outputs =
